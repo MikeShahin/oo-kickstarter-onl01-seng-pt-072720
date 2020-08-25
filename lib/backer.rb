98366163f51e6f79c3cc36e75b92e4965cae2308
Mike Shahindoust <gobias101@gmail.com>
2020-08-25 23:54:28 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,14 +3,14 @@ class Backer
   attr_accessor :backed_projects
   attr_reader :name
   
-  @@backed_projects = []
   
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
   
-  def self.backed_projects
-    @@backed_projects
-  end
+  # def self.backed_projects
+  #   @@backed_projects
+  # end
   
 end
